7fae067f7d473bacd836a1e1b9817bc2b68d723c
John O'Connor <john.oconnor@puppet.com>
2020-04-15 11:59:10 +0100

    (POOLER-160) Add Prometheus Stats Feeds
    Add a new Prometheus class as an additional stats feed along with the
    existing feeds.
    
    Move the metrics initialisation code into its own class and sub-class
    the individual metrics implementations under this.

---
+++ spec/unit/graphite_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+require 'vmpooler/graphite'
+
+describe 'graphite' do
+
+  let(:prometheus) do
+    Vmpooler::Providers.new
+  end
+
+end
---
+++ spec/unit/statsd_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+require 'vmpooler/statsd'
+
+describe 'statsd' do
+
+  let(:providers) do
+    Vmpooler::Providers.new
+  end
+
+end
