ebc9b4dcfd476b22e9c4a9bfbe4bc6c5261d9728
John O'Connor <john.oconnor@puppet.com>
2020-05-07 16:35:48 +0100

    (POOLER-160) Select Endpoint options dynamically
    Selectively load the pooler api and/or Prometheus endpoints.

--- lib/vmpooler/api.rb
+++ lib/vmpooler/api.rb
@@ -2,29 +2,6 @@
 
 module Vmpooler
   class API < Sinatra::Base
-    not_found do
-      content_type :json
-
-      result = {
-        ok: false
-      }
-
-      JSON.pretty_generate(result)
-    end
-
-    use Rack::Deflater
-    use Prometheus::Middleware::Collector, metrics_prefix: 'vmpooler_http'
-    use Prometheus::Middleware::Exporter, path: '/prometheus'
-
-    # Load dashboard components
-    begin
-      require 'dashboard'
-    rescue LoadError
-      require File.expand_path(File.join(File.dirname(__FILE__), 'dashboard'))
-    end
-
-    use Vmpooler::Dashboard
-
     # Load API components
     %w[helpers dashboard reroute v1].each do |lib|
       begin
@@ -33,18 +10,49 @@ module Vmpooler
         require File.expand_path(File.join(File.dirname(__FILE__), 'api', lib))
       end
     end
+    # Load dashboard components
+    begin
+      require 'dashboard'
+    rescue LoadError
+      require File.expand_path(File.join(File.dirname(__FILE__), 'dashboard'))
+    end
 
-    use Vmpooler::API::Dashboard
-    use Vmpooler::API::Reroute
-    use Vmpooler::API::V1
-
-    def self.execute(config, redis, metrics)
+    def self.execute(torun, config, redis, metrics)
       self.settings.set :config, config
-      self.settings.set :redis, redis
+      self.settings.set :redis, redis unless redis.nil?
       self.settings.set :metrics, metrics
       self.settings.set :checkoutlock, Mutex.new
-      # Initialise Prometheus Counters if required.
-      metrics.setup_prometheus_metrics if metrics.respond_to?(:setup_prometheus_metrics)
+
+      # Deflating in all situations
+      # https://www.schneems.com/2017/11/08/80-smaller-rails-footprint-with-rack-deflate/
+      use Rack::Deflater
+
+      # not_found clause placed here to fix rspec test issue.
+      not_found do
+        content_type :json
+
+        result = {
+          ok: false
+        }
+
+        JSON.pretty_generate(result)
+      end
+
+      if metrics.respond_to?(:setup_prometheus_metrics)
+        # Prometheus metrics are only setup if actually specified
+        # in the config file.
+        metrics.setup_prometheus_metrics
+
+        use Prometheus::Middleware::Collector, metrics_prefix: "#{metrics.metrics_prefix}_http"
+        use Prometheus::Middleware::Exporter, path: metrics.endpoint
+      end
+
+      if torun.include? 'api'
+        use Vmpooler::Dashboard
+        use Vmpooler::API::Dashboard
+        use Vmpooler::API::Reroute
+        use Vmpooler::API::V1
+      end
 
       # Get thee started O WebServer
       self.run!
